5838207b88d343276c662ea4e9bbaebde104ce3b
mugdad <alhammad5057@gmail.com>
2024-03-27 06:31:39 +0300

    completed lesson after 2 weeks

--- #16.cpp
+++ #16.cpp
@@ -1,6 +1,6 @@
 /*
   Data Types
-  - What Is Data ?
+  - What Is Data ? بيانات 
   - Data Examples In Real Life
   --- Integer => 5000, 10, -100
   --- String => "Elzero Web School", "Osama Elzero", "100A"
@@ -14,12 +14,33 @@
   What Is Operand ?
   - The Part Of an Instruction Representing The Data Manipulated by The Operator
 */
+/*
+mugdad(string)
+18.5(double||float)
+yes / true etc(boolean)
+"100"(int)
+
+
+
+
+*/
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  cout<<"hell";
+int num= 0;
+string name = "m";
+bool status = true;
+int num1 = 100;
+int num2 = 50;
+cout<<sizeof (num)<<"\n" ;
+cout<<sizeof (name)<<"\n";
+cout<<sizeof (status)<<"\n";
+cout<< num1/num2//can't divide int with string;
+
+
+
 }
     
